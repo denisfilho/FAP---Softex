c8bc25c3e2ab311cd589cdb97f435688424a4ae5
denisfilho <denisferreira93@hotmail.com>
2023-08-02 12:59:42 -0300

    Logica de Programacao - Modulo 3 - Atividade 17

---
+++ Lógica de Programação/Módulo 3/Atividade 17/index.js
@@ -0,0 +1,14 @@
+let nomes=['Ana', 'João', 'Maria', 'Carlos', 'Helena', 'Marcos', 'Júlia', 'Matheus', 'Bia', 'Lucas']
+let idades=[10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+let cores=['rosa', 'azul', 'vermelho', 'preto', 'roxo', 'laranja', 'lilás', 'verde', 'azul', 'vermelho']
+
+console.log(nomes)
+console.log(idades)
+console.log(cores)
+
+idades[2] = 29;
+cores[3] = 'royal'; 
+
+console.log(nomes)
+console.log(idades)
+console.log(cores)
